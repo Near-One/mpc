ded5817afba33296dfb4d3bf4d908c496d7118e5
kevindeforth <kevindeforth@users.noreply.github.com>
2025-03-04 10:31:31 +0100

    finally unittests compile and run

--- libs/chain-signatures/contract/tests/updates.rs
+++ libs/chain-signatures/contract/tests/updates.rs
@@ -3,9 +3,9 @@ use common::{init_env, vote_update_till_completion, CONTRACT_FILE_PATH, INVALID_
 
 use std::collections::HashMap;
 
-use mpc_contract::config::{Config, ProtocolConfig};
-use mpc_contract::errors;
-use mpc_contract::update::{ProposeUpdateArgs, UpdateId};
+use legacy_contract::config::{ConfigV1, ProtocolConfig};
+use legacy_contract::errors;
+use legacy_contract::update::{ProposeUpdateArgs, UpdateId};
 
 use near_workspaces::types::NearToken;
 
@@ -81,12 +81,14 @@ async fn test_propose_update_config() {
         .contains(&errors::VoteError::VoterNotParticipant.to_string()));
 
     // have each participant propose a new update:
-    let new_config = Config {
-        protocol: ProtocolConfig {
-            max_concurrent_generation: 10000,
-            ..ProtocolConfig::default()
-        },
-        ..Config::default()
+    let new_config = ConfigV1 {
+        max_num_requests_to_remove: 5,
+        request_timeout_blocks: 6,
+        //protocol: ProtocolConfig {
+        //    max_concurrent_generation: 10000,
+        //    ..ProtocolConfig::default()
+        //},
+        //..ConfigV1::default()
     };
 
     let mut proposals = Vec::with_capacity(accounts.len());
@@ -109,7 +111,7 @@ async fn test_propose_update_config() {
     }
 
     let old_config: serde_json::Value = contract.view("config").await.unwrap().json().unwrap();
-    let state: mpc_contract::ProtocolContractState =
+    let state: legacy_contract::ProtocolContractState =
         contract.view("state").await.unwrap().json().unwrap();
 
     // check that each participant can vote on a singular proposal and have it reflect changes:
@@ -187,7 +189,7 @@ async fn test_propose_update_contract() {
 
     dbg!(&execution);
 
-    let state: mpc_contract::ProtocolContractState = execution.json().unwrap();
+    let state: legacy_contract::ProtocolContractState = execution.json().unwrap();
     dbg!(state);
 }
 
@@ -225,7 +227,7 @@ async fn test_invalid_contract_deploy() {
         .unwrap();
 
     dbg!(&execution);
-    let state: mpc_contract::ProtocolContractState = execution.json().unwrap();
+    let state: legacy_contract::ProtocolContractState = execution.json().unwrap();
     dbg!(state);
 }
 
@@ -260,7 +262,7 @@ async fn test_propose_update_contract_many() {
     vote_update_till_completion(&contract, &accounts, proposals.last().unwrap()).await;
 
     // Let's check that we can call into the state and see all the proposals.
-    let state: mpc_contract::ProtocolContractState =
+    let state: legacy_contract::ProtocolContractState =
         contract.view("state").await.unwrap().json().unwrap();
     dbg!(state);
 }
