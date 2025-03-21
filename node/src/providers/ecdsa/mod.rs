71387d0931b8744217b37969f5ec119b716db640
kuksag <georgiy.kuksa@gmail.com>
2025-03-21 13:21:08 +0100

    Refactoring: move  `affine_point_to_public_key` to ecdsa provider; remove `hkdf.rs`

--- node/src/providers/ecdsa/mod.rs
+++ node/src/providers/ecdsa/mod.rs
@@ -22,6 +22,8 @@ use cait_sith::{FullSignature, KeygenOutput};
 use k256::{AffinePoint, Scalar, Secp256k1};
 use near_time::Clock;
 use std::sync::Arc;
+use anyhow::Context;
+use k256::elliptic_curve::sec1::ToEncodedPoint;
 use tokio::sync::mpsc;
 
 #[derive(Clone)]
@@ -224,4 +226,11 @@ impl SignatureProvider for EcdsaSignatureProvider {
 
         Ok(())
     }
+}
+
+pub fn affine_point_to_public_key(point: AffinePoint) -> anyhow::Result<near_crypto::PublicKey> {
+    Ok(near_crypto::PublicKey::SECP256K1(
+        near_crypto::Secp256K1PublicKey::try_from(&point.to_encoded_point(false).as_bytes()[1..65])
+            .context("Failed to convert affine point to public key")?,
+    ))
 }
--- node/src/providers/mod.rs
+++ node/src/providers/mod.rs
@@ -9,6 +9,7 @@
 mod ecdsa;
 pub use ecdsa::EcdsaSignatureProvider;
 pub use ecdsa::EcdsaTaskId;
+pub use ecdsa::affine_point_to_public_key;
 
 use crate::network::{MeshNetworkClient, NetworkTaskChannel};
 use crate::sign_request::SignatureId;
