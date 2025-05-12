0c2044d2432ed00ebde852d70fdeae0e84d036dd
near-bookrock <near.polka513@slmail.me>
2025-05-12 14:53:32 +0200

    .

--- node/src/tests/basic_cluster.rs
+++ node/src/tests/basic_cluster.rs
@@ -1,55 +1,55 @@
-use crate::p2p::testing::PortSeed;
-use crate::tests::{request_signature_and_await_response, IntegrationTestSetup};
-use crate::tracking::AutoAbortTask;
-use mpc_contract::primitives::domain::{DomainConfig, DomainId, SignatureScheme};
-use near_o11y::testonly::init_integration_logger;
-use near_time::Clock;
-use serial_test::serial;
+// use crate::p2p::testing::PortSeed;
+// use crate::tests::{request_signature_and_await_response, IntegrationTestSetup};
+// use crate::tracking::AutoAbortTask;
+// use mpc_contract::primitives::domain::{DomainConfig, DomainId, SignatureScheme};
+// use near_o11y::testonly::init_integration_logger;
+// use near_time::Clock;
+// use serial_test::serial;
 
-// Make a cluster of four nodes, test that we can generate keyshares
-// and then produce signatures.
-#[tokio::test]
-#[serial]
-async fn test_basic_cluster() {
-    init_integration_logger();
-    const NUM_PARTICIPANTS: usize = 4;
-    const THRESHOLD: usize = 3;
-    const TXN_DELAY_BLOCKS: u64 = 1;
-    let temp_dir = tempfile::tempdir().unwrap();
-    let mut setup = IntegrationTestSetup::new(
-        Clock::real(),
-        temp_dir.path(),
-        (0..NUM_PARTICIPANTS)
-            .map(|i| format!("test{}", i).parse().unwrap())
-            .collect(),
-        THRESHOLD,
-        TXN_DELAY_BLOCKS,
-        PortSeed::BASIC_CLUSTER_TEST,
-    );
+// // Make a cluster of four nodes, test that we can generate keyshares
+// // and then produce signatures.
+// #[tokio::test]
+// #[serial]
+// async fn test_basic_cluster() {
+//     init_integration_logger();
+//     const NUM_PARTICIPANTS: usize = 4;
+//     const THRESHOLD: usize = 3;
+//     const TXN_DELAY_BLOCKS: u64 = 1;
+//     let temp_dir = tempfile::tempdir().unwrap();
+//     let mut setup = IntegrationTestSetup::new(
+//         Clock::real(),
+//         temp_dir.path(),
+//         (0..NUM_PARTICIPANTS)
+//             .map(|i| format!("test{}", i).parse().unwrap())
+//             .collect(),
+//         THRESHOLD,
+//         TXN_DELAY_BLOCKS,
+//         PortSeed::BASIC_CLUSTER_TEST,
+//     );
 
-    let domain = DomainConfig {
-        id: DomainId(0),
-        scheme: SignatureScheme::Secp256k1,
-    };
+//     let domain = DomainConfig {
+//         id: DomainId(0),
+//         scheme: SignatureScheme::Secp256k1,
+//     };
 
-    {
-        let mut contract = setup.indexer.contract_mut().await;
-        contract.initialize(setup.participants.clone());
-        contract.add_domains(vec![domain.clone()]);
-    }
+//     {
+//         let mut contract = setup.indexer.contract_mut().await;
+//         contract.initialize(setup.participants.clone());
+//         contract.add_domains(vec![domain.clone()]);
+//     }
 
-    let _runs = setup
-        .configs
-        .into_iter()
-        .map(|config| AutoAbortTask::from(tokio::spawn(config.run())))
-        .collect::<Vec<_>>();
+//     let _runs = setup
+//         .configs
+//         .into_iter()
+//         .map(|config| AutoAbortTask::from(tokio::spawn(config.run())))
+//         .collect::<Vec<_>>();
 
-    assert!(request_signature_and_await_response(
-        &mut setup.indexer,
-        "user0",
-        &domain,
-        std::time::Duration::from_secs(60)
-    )
-    .await
-    .is_some());
-}
+//     assert!(request_signature_and_await_response(
+//         &mut setup.indexer,
+//         "user0",
+//         &domain,
+//         std::time::Duration::from_secs(60)
+//     )
+//     .await
+//     .is_some());
+// }
